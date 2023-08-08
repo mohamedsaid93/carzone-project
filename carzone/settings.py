d8b6ac048dfcb30acadfddb65c7fecae0ee570f0
Mohamed Said <msm.793@hotmail.com>
2023-08-08 17:58:11 +0100

    Completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -121,3 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carzone/static')
+]
+
+
+# Media Settings
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
+from .models import Team
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'pages/home.html')
+    teams = Team.objects.all()
+
+    context = {
+        'teams': teams
+    }
+
+    return render(request, 'pages/home.html', context)
+
+#########################################################################
+
+def about(request):
+    teams = Team.objects.all()
+
+    context = {
+        'teams': teams
+    }
+
+    return render(request, 'pages/about.html', context)
+
+#########################################################################
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+##########################################################################
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+##########################################################################
